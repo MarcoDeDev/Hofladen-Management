82772db5a4e9284cdf8d90da5298943afcf4f89f
MarcoDeDev <d.marco.86.ae@gmail.com>
2025-08-18 10:42:21 +0200

    Spring-Boot Projekt initialisiert

---
+++ src/main/java/com/farmit/kartoffelsoft_backend/KartoffelsoftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.farmit.kartoffelsoft_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartoffelsoftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartoffelsoftBackendApplication.class, args);
+	}
+
+}
