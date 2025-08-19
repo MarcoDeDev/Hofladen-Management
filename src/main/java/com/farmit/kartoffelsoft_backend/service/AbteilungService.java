871351147fb1741f9454b8bae2c7c1565947edda
MarcoDeDev <d.marco.86.ae@gmail.com>
2025-08-19 10:49:11 +0200

    AbteilungService - AbteilungServiceImpl

--- src/main/java/com/farmit/kartoffelsoft_backend/service/AbteilungService.java
+++ src/main/java/com/farmit/kartoffelsoft_backend/service/AbteilungService.java
@@ -1,4 +1,16 @@
 package com.farmit.kartoffelsoft_backend.service;
 
+import com.farmit.kartoffelsoft_backend.model.Abteilung;
+
+import java.util.List;
+
 public interface AbteilungService {
+
+    Abteilung save(Abteilung abteilung);
+
+    Abteilung getAbteilungById(long id);
+
+    List<Abteilung> getAllAbteilung();
+
+    void deleteAbteilungById(long id);
 }
--- src/main/java/com/farmit/kartoffelsoft_backend/service/AbteilungServiceImpl.java
+++ src/main/java/com/farmit/kartoffelsoft_backend/service/AbteilungServiceImpl.java
@@ -1,4 +1,35 @@
 package com.farmit.kartoffelsoft_backend.service;
 
-public class AbteilungServiceImpl {
+import com.farmit.kartoffelsoft_backend.model.Abteilung;
+import com.farmit.kartoffelsoft_backend.repository.AbteilungRepository;
+
+import java.util.List;
+
+public class AbteilungServiceImpl implements AbteilungService{
+
+    private final AbteilungRepository abteilungRepository;
+
+    public AbteilungServiceImpl(AbteilungRepository abteilungRepository) {
+        this.abteilungRepository = abteilungRepository;
+    }
+
+    @Override
+    public Abteilung save(Abteilung abteilung) {
+        return null;
+    }
+
+    @Override
+    public Abteilung getAbteilungById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Abteilung> getAllAbteilung() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteAbteilungById(long id) {
+
+    }
 }
