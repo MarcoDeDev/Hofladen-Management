f9038bf0434cbc1b0618a4a9da513147ed03251d
grzegorz <grzegorzchlebicz97@gmail.com>
2025-08-19 12:04:02 +0200

    Service - ServiceImpl 6-9 Korrektur

--- src/main/java/com/farmit/kartoffelsoft_backend/service/TeilDerLieferungServiceImpl.java
+++ src/main/java/com/farmit/kartoffelsoft_backend/service/TeilDerLieferungServiceImpl.java
@@ -1,11 +1,19 @@
 package com.farmit.kartoffelsoft_backend.service;
 
 import com.farmit.kartoffelsoft_backend.model.TeilDerLieferung;
+import com.farmit.kartoffelsoft_backend.repository.TeilDerLieferungRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 public class TeilDerLieferungServiceImpl implements TeilDerLieferungService {
+
+    private final TeilDerLieferungRepository teilDerLieferungRepository;
+
+    public TeilDerLieferungServiceImpl(TeilDerLieferungRepository teilDerLieferungRepository) {
+        this.teilDerLieferungRepository = teilDerLieferungRepository;
+    }
+
     @Override
     public TeilDerLieferung save(TeilDerLieferung teilDerLieferung) {
         return null;
